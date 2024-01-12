0ede75cb08d0155fbc84e368667f80ebe7f9c134
taruna98 <taruna.ersa@gmail.com>
2024-01-13 00:16:33 +0700

    templating ui sidebar and navbar

--- resources/views/layout/admin_header.blade.php
+++ resources/views/layout/admin_header.blade.php
@@ -3,12 +3,12 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="{{ URL::asset('assets/img/logo.png') }}" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <span class="d-none d-lg-block">Opsadmin</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
-    <div class="search-bar">
+    <div class="search-bar d-none">
       <form class="search-form d-flex align-items-center" method="POST" action="#">
         <input type="text" name="query" placeholder="Search" title="Enter search keyword">
         <button type="submit" title="Search"><i class="bi bi-search"></i></button>
@@ -18,13 +18,13 @@
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
 
-        <li class="nav-item d-block d-lg-none">
+        <li class="nav-item d-none d-lg-none">
           <a class="nav-link nav-icon search-bar-toggle " href="#">
             <i class="bi bi-search"></i>
           </a>
         </li><!-- End Search Icon-->
 
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown d-none">
 
           <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
             <i class="bi bi-bell"></i>
@@ -99,7 +99,7 @@
 
         </li><!-- End Notification Nav -->
 
-        <li class="nav-item dropdown">
+        <li class="nav-item dropdown d-none">
 
           <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
             <i class="bi bi-chat-left-text"></i>
@@ -181,33 +181,33 @@
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            <li class="d-none">
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
             </li>
-            <li>
+            <li class="d-none">
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            <li class="d-none">
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-gear"></i>
                 <span>Account Settings</span>
               </a>
             </li>
-            <li>
+            <li class="d-none">
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            <li class="d-none">
               <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
                 <i class="bi bi-question-circle"></i>
                 <span>Need Help?</span>
               </a>
             </li>
-            <li>
+            <li class="d-none">
               <hr class="dropdown-divider">
             </li>
 
---
+++ resources/views/layout/kretech_sidebar.blade.php
@@ -0,0 +1,24 @@
+<li class="nav-heading">PAGE</li>
+
+<li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+        <i class="bi bi-menu-button-wide"></i><span>Contents</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <li>
+            <a href="">
+                <i class="bi bi-circle"></i><span>Profile</span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="bi bi-circle"></i><span>Portfolio</span>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="bi bi-circle"></i><span>Article</span>
+            </a>
+        </li>
+    </ul>
+</li><!-- End Contents Nav -->
